486046b7b3b2465971e60412434deca6f6f5b1a1
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-05-24 08:45:25 +0100

    comited build folder

--- server/build/index.js
+++ server/build/index.js
@@ -11,27 +11,34 @@ const cors_1 = __importDefault(require("cors"));
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+// CORS options
 const corsOptions = {
     origin: ['https://asinhonore-projectmanagement-web-app.netlify.app', 'http://localhost:5173'],
     credentials: true,
 };
+// Middleware
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use('/v2/auth', authRoutes_1.default);
+// MongoDB connection
 const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 5050;
 if (!mongoURI) {
     throw new Error('MongoDB connection string is not defined. Check your .env file.');
 }
-mongoose_1.default
-    .connect(mongoURI)
+mongoose_1.default.connect(mongoURI)
     .then(() => {
     console.log('Connected to MongoDB successfully 👍');
+    // Server listening
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
 })
     .catch((error) => {
     console.error('Failed to connect to MongoDB:', error);
+});
+// Route to send "Hello, world!"
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
 });
